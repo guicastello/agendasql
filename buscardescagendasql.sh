739602254e4b769b3beec6eb9069683e4975a859
guicastello <guicastello@gmail.com>
2019-08-24 00:50:42 -0300

    Ajuste de parametros para que le guste a Guido :-P

--- buscardescagendasql.sh
+++ buscardescagendasql.sh
@@ -3,16 +3,4 @@ echo "======================+++++======="
 echo " Busca por DEscripcion Agenda-SQL "
 echo "===========================+++++=="
 echo -ne "Descripcion: ";read descripcion
-# echo -ne "Usuario: "; read usuario
-
-echo "SELECT * FROM agenda.calendario WHERE descripcion  like '%"${descripcion}"%';" > /tmp/buscafechaagenda.sql
-mysql agenda < /tmp/buscafechaagenda.sql > salida.txt
-rm /tmp/buscafechaagenda.sql
-
-if  [[ -s salida.txt ]]; then
-	cat salida.txt;
-else
-	echo "No existen registros.";
-fi
-rm salida.txt
-
+mysql agenda -e "SELECT * FROM calendario WHERE descripcion like '%"${descripcion}"%';"
--- buscarfechaagendasql.sh
+++ buscarfechaagendasql.sh
@@ -3,16 +3,4 @@ echo "============================="
 echo " Busca por fecha Agenda-SQL "
 echo "============================="
 echo -ne "fecha: ";read fecha
-# echo -ne "Usuario: "; read usuario
-
-echo "SELECT * FROM agenda.calendario WHERE fecha like '%"${fecha}"%';" > /tmp/buscafechaagenda.sql
-mysql agenda < /tmp/buscafechaagenda.sql > salida.txt
-rm /tmp/buscafechaagenda.sql
-
-if  [[ -s salida.txt ]]; then
-	cat salida.txt;
-else
-	echo "No existen registros.";
-fi
-rm salida.txt
-
+mysql agenda -e "SELECT * FROM agenda.calendario WHERE fecha like '%"${fecha}"%';"
