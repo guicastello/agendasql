d3a4796476ef1a8ecaf3225f57a882d520173074
guicastello <guicastello@gmail.com>
2019-08-31 15:28:25 -0300

    modulo tareas

--- crea_tabla_calendario.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE calendario (
-	id int(8) not null auto_increment,
-	fecha DATE,
-	hora_ini TIME,
-	hora_fin TIME,
-	todo_dia BOOLEAN,
-	descripcion VARCHAR(256),
-	PRIMARY KEY (id)
-);
--- tareassql.sh
+++ tareassql.sh
@@ -11,7 +11,7 @@ subr="\033[4m"					# Letras subrayadas
 hoy="$(date +%Y-%m-%d)"			# obtengo la fecha del día de hoy (YYYY/MM/DD)
 efe="$(date +%m-%d)"			# fecha dia/mes para efemerides
 VERSION="V1.0"					# Version
-LOG="agendasql-"$VERSION".log"	# Nombre del log 
+LOG="tareassql-"$VERSION".log"	# Nombre del log 
 
 ### Definicion de Funciones ###
 #
@@ -43,11 +43,11 @@ LOG="agendasql-"$VERSION".log"	# Nombre del log
 		cal
 		echo -ne "\033[4;21H | "
 		echo -ne "\033[5;21H | "
-		echo -e " 1) Alta\t\t\t4) Busca x fecha\t7) Backup"
+		echo -e " 1) Alta\t\t\t4) Busca x fecha fin\t7) Modifica registro"
 		echo -ne "\033[6;21H | "
-		echo -e " 2) Borra fecha\t\t5) Agenda para Hoy\t8) Modificar Registro"
+		echo -e " 2) Borra tarea\t\t5) Tareas para Hoy"
 		echo -ne "\033[7;21H | "
-		echo -e " 3) Borra x Descripcion\t6) Efemerides\t\t9) Salir"
+		echo -e " 3) Busca x fecha inicio\t6) Backup\t\t9) Salir"
 		echo -ne "\033[8;21H | "
 		echo -ne "\033[9;21H |  opción: "
 		echo -ne "\033[s" # grabo posision del cursor
@@ -75,85 +75,90 @@ while [ $opcion != "9" ]; do
 
 	1) mcur;
 	   echo -e "> Alta registro";
-	   echo -ne "fecha: ";read fecha;
+	   echo -ne "Titulo: "; read titulo;
+	   echo -ne "Prioridad: "; read prioridad;
+	   echo -ne "fecha inicio: ";read fecha_ini;
 	   echo -ne "hora inicio: "; read hini;
+	   echo -ne "fecha fin:"; read fecha_fin;
 	   echo -ne "hora fin: "; read hfin;
-	   echo -ne "todo el dia: "; read tld;
-	   echo -ne "Descripcion: "; read desc;
-	   mysql agenda -e "insert into agenda.calendario (fecha, hora_ini, hora_fin, todo_dia, descripcion) values ('${fecha}', '${hini}', '${hfin}', '${tld}', '${desc}');";
+	   echo -ne "Completa: "; read completada;
+	   echo -ne "Descripcion: "; read Notas;
+	   mysql agenda -e "insert into agenda.tareas (titulo, prioridad,fecha_ini,hora_ini,fecha_fin,hora_fin,completada,Notas) values ('${titulo}', '${prioridad}', '${fecha_ini}', '${hora_ini}', '${fecha_fin}', '${hora_fin}', '${completada}', '${Notas}');";
        pausa;
        2LOG "Alta de registro.";;
 
 	2) mcur;
-	   echo -e " > Borrar registro por fecha";
-	   echo -ne "Fecha: ";read fecha;
-	   mysql agenda -e "select * from calendario where fecha like '%"${fecha}"%';";
+	   echo -e " > Borrar registro por titulo";
+	   echo -ne "Fecha: ";read titulo;
+	   mysql agenda -e "select * from tareas where fecha like '%"${titulo}"%';";
            echo -ne "Ingrese Id a Borrar: "; read id;
-           mysql agenda -e "DELETE FROM calendario WHERE  id = "${id}";";
-           mysql agenda -e "select * from calendario WHERE fecha like '%"${fecha}"%';";
+           mysql agenda -e "DELETE FROM tareas WHERE  id = "${id}";";
+           mysql agenda -e "select * from tareas WHERE fecha like '%"${titulo}"%';";
 	   pausa;
-	   2LOG "Borrado fecha "$fecha;;
+	   2LOG "Borrado titulo "$titulo;;
 
 	3) mcur;
-	   echo -e " > Borrar registro por descripcion";
-	   echo -ne "Descripcion: ";read descripcion;
-	   mysql agenda -e "SELECT * FROM calendario WHERE description LIKE '%"${descripcion}"%';";
-	   echo -e "Ingrese Id a Borrar> "; read id;
-           mysql agenda -e "DELETE FROM calendario WHERE id = "${id}";";
-	   pausa;
-	   2LOG "Borrado descripcion"$CALENDAR;;
+	   echo " > Tareas para el  dia de hoy";
+	   echo -e "	- Tareas que empiezan:\n";
+       mysql agenda -e "SELECT * FROM tareas WHERE fecha_ini like '%"${hoy}"%';";
+       echo -e "\033[41m	- Tareas vencidas:\n";
+       mysql agenda -e "SELECT * FROM tareas WHERE fecha_ini (BETWEEN '2019-01-01' '%"${hoy}"%' AND completada = 'FALSE');"; 
+       echo -e $rset;   
+       pausa;
+       2LOG "Agenda de Hoy";;
 
 	4) mcur;
-	   echo -e " > Busqueda por fecha";
-	   echo -ne "fecha: ";read fecha;
-	   mysql agenda -e "SELECT * FROM calendario WHERE fecha like '%"${fecha}"%';";
+	   echo -e " > Busqueda por fecha de fin";
+	   echo -ne "fecha fin: ";read fecha_ini;
+	   mysql agenda -e "SELECT * FROM tareas WHERE fecha_fin like '%"${fecha_fin}"%';";
 	   pausa;
-	   2LOG "Busqueda Fecha "$fecha;;
+	   2LOG "Busqueda de tareas por Fecha de Inicio"$fecha_fin;;
 
 	5) mcur;
-	   echo " > Agenda para el  dia de hoy";
-           mysql agenda -e "SELECT * FROM calendario WHERE fecha like '%"${hoy}"%';";
-           pausa;
-           2LOG "Agenda de Hoy";;
+	   echo -e " > Busqueda por fecha de inicio\n";
+	   mysql agenda -e "SELECT * FROM tareas WHERE fecha_ini like '%"${hoy}"%';";
+	   pausa;
+	   2LOG "Busqueda de tareas por Fecha de Inicio"$fecha_ini;;
 
 	6) mcur;
-	   echo -e " > Efemerides";
-	   mysql agenda -e "SELECT * FROM calendario WHERE (fecha LIKE '%"${efe}"%' AND todo_dia = TRUE);";
-	   pausa;
-	   2LOG "Efemerides";;
-
-	7) mcur;
 	   echo -e " > Backup agendasql";
 	   mysqldump agenda > ~/agendasql/backups/agenda_`date +%Y-%m-%d_%H%M`.sql;
 	   echo -e $bold"Backup OK"$rset;
 	   pausa;
 	   2LOG "Back up agenda";;
 
-	8) mcur;
-	   echo -ne "fecha: ";read fecha;
-	   mysql agenda -e "SELECT * FROM calendario WHERE fecha like '%"${fecha}"%';";
+	7) mcur;
+	   echo -ne "fecha inicio: ";read fecha_ini;
+	   mysql agenda -e "SELECT * FROM tareas WHERE fecha_ini like '%"${fecha_ini}"%';";
 	   echo -e " > Elija registro a modificar";
 	   echo -ne "id: "; read id;
 	   echo -e " > Elija campo a modificar:\n";
-	   echo -e $rvrs"1"$rset" fecha "$rvrs"2"$rset" hora inicio "$rvrs"3"$rset" hora fin "$rvrs"4"$rset" todo el dia "$rvrs"5"$rset" descripcion "$rvrs"6"$rset" Cancelar\n";
+	   echo -e $rvrs"1"$rset" titulo "$rvrs"2"$rset" prioridad "$rvrs"3"$rset" fecha inicio "$rvrs"4"$rset" fecha fin "$rvrs"5"$rset" hora inicio "$rvrs"6"$rset" hora fin "$rvrs"7"$rset" completada\n";
+	   echo -e $rvrs"8"$rset" notas "$rvrs"9"$rset" cancelar";
 	   echo -ne "opcion: ";read opcion;
 	   case $opcion in
-		1) echo -ne "nueva fecha: ";read fecha
-		   mysql agenda -e "UPDATE calendario SET fecha = ${fecha} WHERE id = ${id} ;";;
-		2) echo -ne "nueva hora inicio: "; read hora_ini;
-		   mysql agenda -e "UPDATE calendario SET hora_ini = ${hora_ini} WHERE id = ${id};";;
-		3) echo -ne "nueva hora fin: "; read hora_fin;
-		   mysql agenda -e "UPDATE calendario SET hora_fin = ${hora_fin} WHERE id = ${id};";;
-		4) echo -ne "nueva todo_dia: "; read todo_dia;
-		   mysql agenda -e "UPDATE calendario SET todo_dia = ${todo_dia} WHERE id = ${id};";;
-		5) echo -ne "nueva descripcion: "; read descripcion;
-		   mysql agenda -e "UPDATE calendario SET descripcion = ${descripcion} WHERE id = ${id};";;
+		1) echo -ne "nueva titulo: ";read titulo;
+		   mysql agenda -e "UPDATE tareas SET titulo = ${titulo} WHERE id = ${id} ;";;
+		2) echo -ne "nueva prioridad: "; read prioridad;
+		   mysql agenda -e "UPDATE tareas SET prioridad = ${prioridad} WHERE id = ${id};";;
+		3) echo -ne "nueva fecha inicio: "; read fecha_ini;
+		   mysql agenda -e "UPDATE tareas SET fecha_ini = ${fecha_ini} WHERE id = ${id};";;
+		4) echo -ne "nueva fecha fin: "; read fecha_fin;
+		   mysql agenda -e "UPDATE tareas SET fecha_fin = ${fecha_fin} WHERE id = ${id};";;
+		5) echo -ne "nueva hora inicio: "; read hora_ini;
+		   mysql agenda -e "UPDATE tareas SET hora_ini = ${hora_ini} WHERE id = ${id};";;
+		6) echo -ne "nueva hora fin: "; read hora_ini;
+		   mysql agenda -e "UPDATE tareas SET hora_fin = ${hora_fin} WHERE id = ${id};";;
+		7) echo -ne "nueva completada: "; read completada;
+		   mysql agenda -e "UPDATE tareas SET completada = ${completada} WHERE id = ${id};";;
+		8) echo -ne "nueva hora inicio: "; read notas;
+		   mysql agenda -e "UPDATE tareas SET Notas = ${notas} WHERE id = ${id};";;
 	   esac
-	   mysql agenda -e "SELECT * FROM calendario WHERE id  = '$id' ;"
+	   mysql agenda -e "SELECT * FROM tareas WHERE id  = '$id' ;"
        pausa;;
   esac
 done
-2LOG "Fin de sessión "$(whoami)" en agendasql "
+2LOG "Fin de sessión "$(whoami)" en tareassql "
 echo -e "\033[13;1H"
 #
 #-----------------------------------------------------------------------------------------------------------------
@@ -162,5 +167,6 @@ echo -e "\033[13;1H"
 
 # 2019/08/24	Version 1.0
 #		* Puesta en marcha del modulo integrado
-#
+# 2019/08/27
+#		* Creacion modilo de tareas
 exit 0
