603929c942cc558c92d2fd4854ed7b34a5189fda
guicastello <guicastello@gmail.com>
2019-09-07 11:28:32 -0300

    commit

---
+++ modulos/crea_base.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE agenda;
+CREATE TABLE calendario (
+	fecha DATE,
+	hora_ini TIME,
+	hora_fin TIME,
+	todo_dia BOOLEAN,
+	descripcion VARCHAR(256),
+	PRIMARY KEY (id)
+);CREATE TABLE tareas (
+	id int(8) not null auto_increment,
+	titulo varchar(64),
+	prioridad int(1),
+	fecha_ini date),
+	fecha_fin date,
+	hora_ini time,
+	hora_fin time,
+	completada boolean,
+	Notas varchar(512),
+	PRIMARY KEY (id)
+);
